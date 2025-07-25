d974348bd19d65519f74d79e82a3cdac2cad628c
LJM <dlwhdals1665@gmail.com>
2025-07-25 10:53:08 +0900

    10:53

--- SpringBootBoard_storage/src/main/java/com/cx/board/controller/BoardController.java
+++ SpringBootBoard_storage/src/main/java/com/cx/board/controller/BoardController.java
@@ -1,24 +1,19 @@
 package com.cx.board.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.cx.board.config.BucketConfig;
 import com.cx.board.config.FileUploadConfig;
 import com.cx.board.entity.BoardEntity;
@@ -43,92 +38,57 @@ public class BoardController {
         this.bucketConfig = bucketConfig;
     }
 
-	@PostMapping("write")
-	public String write(@RequestParam String title, @RequestParam String content, HttpSession session,
-			@RequestParam MultipartFile image) {
-		String imgPath = "";
-		if (!image.isEmpty()) {
+	@PostMapping("/write")
+	public String write(@RequestParam String title, @RequestParam String content,
+			HttpSession session, @RequestParam MultipartFile image, Model model) {
+	
+		BoardEntity entity = new BoardEntity();
+		
+		if(!image.isEmpty()) {
+			// 이미지의 이름 
 			String img_name = image.getOriginalFilename();
-			String file_name = UUID.randomUUID() + "_" + img_name;
-			String uploadDir = fileUploadConfig.getUploadDir();
-			String filePath = Paths.get(uploadDir, file_name).toString();
-
+			
+			//java 안에 고유 번호를 만드는 객체 --UUID
+			//이미지의 고유 이름 부여 
+			String file_name = UUID.randomUUID() + "_" +img_name;
+			
 			try {
-				image.transferTo(new File(filePath));
-				imgPath = "/upload/" + file_name;
-			} catch (IllegalStateException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
+				 ObjectMetadata metadata = new ObjectMetadata();
+	        metadata.setContentLength(image.getSize());
+	        metadata.setContentType(image.getContentType());
+
+	        PutObjectRequest request = new PutObjectRequest(bucketConfig.getbucketName(), file_name, image.getInputStream(), metadata)
+	                .withCannedAcl(CannedAccessControlList.PublicRead); // public 접근 허용
+
+	        amazonS3.putObject(request);
+	        String imgPath = amazonS3.getUrl(bucketConfig.getbucketName(), file_name).toString();
+			
+					entity.setTitle(title);
+					entity.setContent(content);
+					entity.setImgPath(imgPath);
+
+					// writer --session에서 가지고 오기 -- down casting
+					UserEntity user = (UserEntity) session.getAttribute("user");
+		
+					String writer = user.getUserId();
+		
+					entity.setWriter(writer);
+			}catch (Exception e) {
 				e.printStackTrace();
 			}
+					
 		}
-		BoardEntity entity = new BoardEntity();
-		entity.setTitle(title);
-		entity.setContent(content);
-		entity.setImgPath(imgPath);
-
-		UserEntity user = (UserEntity) session.getAttribute("user");
-		String writer = user.getUserId();
-		entity.setWriter(writer);
-		BoardEntity result = boardService.write(entity);
-		if (result != null) {
+		
+			BoardEntity result = boardService.write(entity);
+		
+		model.addAttribute("imageUrl", entity.getImgPath());
+		if(result != null) {
+			// 성공
+			//글이 작성이 될 시 index페이지로 이동
 			return "redirect:/";
-		} else {
+		}else {
 			return "redirect:/board/write";
 		}
-	}
-
-	@GetMapping("/detail/{id}")
-	public String detail(@PathVariable Long id, Model model) {
-		System.out.println(id);
-		Optional<BoardEntity> entity = boardService.detail(id);
-		if (entity.isPresent()) {
-			model.addAttribute("detail", entity.get());
-			return "detail";
-		} else {
-			return "redirect:/";
-		}
-	}
-
-	@GetMapping("/edit/{id}")
-	public String edit(@PathVariable Long id, Model model) {
-		Optional<BoardEntity> entity = boardService.detail(id);
-		model.addAttribute("edit", entity.get());
-		return "edit";
-	}
-
-	@PostMapping("/update")
-	public String update(@RequestParam Long id, @RequestParam String title, @RequestParam String content,
-			@RequestParam String oldImgPath, @RequestParam MultipartFile image) {
-
-		Optional<BoardEntity> board = boardService.detail(id);
-		BoardEntity entity = board.get();
-		String uploadDir = fileUploadConfig.getUploadDir();
-
-		if (!image.isEmpty()) {
-			if (oldImgPath != null && !oldImgPath.isEmpty()) {
-				String oldFile = oldImgPath.replace("/upload/", "");
-				Path oldFilePath = Paths.get(uploadDir, oldFile);
-				try {
-					Files.deleteIfExists(oldFilePath);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			try {
-				String newFileName = UUID.randomUUID() + "_" + image.getOriginalFilename();
-				Path newFilePath = Paths.get(uploadDir, newFileName);
-				image.transferTo(newFilePath.toFile());
-				entity.setImgPath("/upload/" + newFileName);
-			} catch (IllegalStateException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		entity.setContent(content);
-		entity.setTitle(title);
-		boardService.write(entity);
-		return "redirect:/board/detail/" + id;
+		
 	}
 }
